6c9c4df4bda1a3bc8c633ab16323ab146d788a8a
Prerna <prerna1998.ps@gmail.com>
2020-07-21 22:34:06 +0530

    Initial Setup for end-to-end API

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
+const Dishes = require('./models/dishes')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
