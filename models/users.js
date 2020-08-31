7ff58ed1d7c0443339b6dc56d6c8e24f73796c9b
Prerna <prerna1998.ps@gmail.com>
2020-09-01 00:39:40 +0530

    User Signup

--- models/users.js
+++ models/users.js
@@ -19,4 +19,4 @@ const userSchema =new Schema(
     }
 );
 
-module.export = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,48 @@
 var express = require('express');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
 const Users = require('../models/users');
+
 var router = express.Router();
+router.use(bodyParser.json());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/signup', (req, res, next) => {
+	Users.findOne({username : req.body.username})
+	.then( (user) => {
+		if(user){
+			let err =new Error('User with username ' + req.body.username + ' already exists!');
+			err.status = 403;
+			next(err);
+		}
+		else{
+			Users.create({
+				username : req.body.username,
+				password : req.body.password
+			})
+			.then( (user) => {
+				res.statusCode=200;
+				res.setHeader('Content-Type', 'application/json');
+				let response ={
+					status : 'Registration Successful',
+					user : user
+				};
+				res.json(response);
+			}, (err) => {
+				next(err);
+			})
+		}
+	}, (err) => {
+		next(err);
+	})
+	.catch((err) => {
+		next(err);
+	})
+})
+
 
 module.exports = router;
